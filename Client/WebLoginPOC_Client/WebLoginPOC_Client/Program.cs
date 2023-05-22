36c3dca400e8d2a3fdedab4cd2744b356fc83a7a
Visqi <2282219-Visqi@users.noreply.gitlab.com>
2023-05-22 21:41:18 +0200

    Upload PoC

---
+++ Client/WebLoginPOC_Client/WebLoginPOC_Client/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace WebLoginPOC_Client
+{
+    public class Program
+    {
+        private const string app_protocol = "custompoc";
+        
+        static void Main(string[] args)
+        {
+            var exe = Assembly.GetExecutingAssembly().Location;
+            exe = exe.Replace(".dll", ".exe");
+
+            if (args.Length == 0)
+            {
+                using (var ident = WindowsIdentity.GetCurrent())
+                {
+                    var principal = new WindowsPrincipal(ident);
+                    if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+                    {
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = exe,
+                            Arguments = string.Join(" ", args),
+                            UseShellExecute = true,
+                            Verb = "runas"
+                        };
+
+                        try
+                        {
+                            var proc = Process.Start(startInfo);
+                            proc?.WaitForExit();
+                            Environment.Exit(0);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("This program must be run as an administrator.");
+                        }
+
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+            }
+
+            string exePath = $"\"{exe}\" \"%1\"";
+
+
+            if (args.Length == 0)
+            {
+                using (var key = Registry.ClassesRoot.OpenSubKey($"{app_protocol}\\shell\\open\\command"))
+                {
+                    if (key == null || key.GetValue(null).ToString() != exePath)
+                    {
+                        Registry.ClassesRoot.DeleteSubKeyTree(app_protocol, false);
+
+                        using (var newKey = Registry.ClassesRoot.CreateSubKey(app_protocol))
+                        {
+                            newKey.SetValue("", "URL:Custom Web Login Protocol POC");
+                            newKey.SetValue("URL Protocol", "");
+
+                            using (var commandKey = newKey.CreateSubKey(@"shell\\open\\command"))
+                            {
+                                commandKey.SetValue("", exePath);
+                            }
+
+                            Console.WriteLine("Registry Created");
+                        }
+                    }
+                }
+            }
+
+            if (args.Length > 0)
+            {
+                var uri = new Uri(args[0]);
+
+                if (uri.Scheme == app_protocol && uri.Host == "launch")
+                {
+                    var token = uri.AbsolutePath.Trim('/');
+                    Console.WriteLine($"Received POC Token: {token}");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+#Starting a Desktop Application from the Web (PoC) (Windows Only)
+
+##Instruction:
+* Compile the Client Project
+* Run the Client Project to Add the custom protocol to the registry.
+* Click on the Link on the web with the custom protocol
+* The Client should launch with the "Token" Parameter.
